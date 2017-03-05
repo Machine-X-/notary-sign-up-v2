d570f8f4dd412b47e4797db0c553acc9530da698
Tyler <Tyler>
2017-03-05 13:03:14 -0500

    Adding bootstrap

--- notary-sign-up/src/app/app.module.ts
+++ notary-sign-up/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TestCompComponent } from './test-comp/test-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCompComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
